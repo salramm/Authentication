96e5f8eea2c26f2d8aa22c82a59b22f1d9b37d28
salramm <1salmanov.ramil@gmail.com>
2021-10-19 12:53:18 -0400

    initiating rest API

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express(); // Now I have the ability to create routes
+const mongoose = require('mongoose');
+
+//Creating routes
+app.get('/', (req,res) => {
+    
+})
+
+
+
+
+
+// Start listening to the server 
+app.listen (3000);
+
+
