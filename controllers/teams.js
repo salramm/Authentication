a5fe13d214668c05e6e28022657b355ef9e313d5
salramm <1salmanov.ramil@gmail.com>
2021-10-22 13:15:42 -0400

    Added basic crud fucntionality on the teams

--- controllers/teams.js
+++ controllers/teams.js
@@ -7,16 +7,33 @@ const Team = require('../models/Team');
 // @desc        get all teams
 // @route       GET /api/v1/teams
 // @access      Public
-exports.getTeams = (req, res, next) => {
-    res.status(200).json({success: true, msg: 'Show all teams', hello: req.hello});
+exports.getTeams = async (req, res, next) => {
+
+    try {
+        const teams = await Team.find();
+        res.status(200).json({success: true, count: teams.length, data: teams});
+    } catch (error) {
+        res.status(400).json({success: false })
+    }
+    // Was previously here: res.status(200).json({success: true, msg: 'Show all teams', hello: req.hello});
 }
 
 
 // @desc        get a single teams
 // @route       GET /api/v1/teams/:id
 // @access      Public
-exports.getTeam = (req, res, next) => {
-    res.status(200).json({success: true, msg: `Show team ${req.params.id}` })
+exports.getTeam = async (req, res, next) => {
+
+    try {
+        const team = await Team.findById(req.params.id); //req.params.id will get the id form the request
+        res.status(200).json({success: true, data: team});
+        if (!team) {
+            return res.status(400).json({success:false, msg: "The ID does not exist"}) //Thsi will handle the rejection if the ID does not exist but is correctly formatted
+        }
+    } catch (error) {
+        res.status(400).json({success: false}); //This handles wrong format requests
+    }
+    // res.status(200).json({success: true, msg: `Show team ${req.params.id}` })
 }
 
 
@@ -24,33 +41,57 @@ exports.getTeam = (req, res, next) => {
 // @route       POST /api/v1/teams
 // @access      Private
 exports.createTeam = async (req, res, next) => {
-    console.log(req.body); /* To see what data is coming from the client in the request but in order to this we need 
+    /* console.log(req.body);  To see what data is coming from the client in the request but in order to this we need 
     to add a middleware that is included with express - do this in server.js */
-    res.status(200).json({success: true, msg: 'Create a new team'});
-    const team = await Team.create(req.body); //We take the entered info in body and pass into the create method on our model
+    // res.status(200).json({success: true, msg: 'Create a new team'});
+    try {  // Try catch will help handle promise rejections; the code will not hang and end with success: flase
+        const team = await Team.create(req.body); //We take the entered info in body and pass into the create method on our model
 
-    res.status(201).json({
-        success: true, data: team
-    })
+        res.status(201).json({
+            success: true, data: team
+        })
+        
+    } catch (error) {
+
+        res.status(400).json({success: false})
+        
+    }
 }
 
 
 // @desc        Update a teams
 // @route       PUT /api/v1/teams/:id
 // @access      Private
-exports.updateTeam = (req, res, next) => {
-    res
-        .status(200)
-        .json({success: true, msg: `Update team ${req.params.id}`})
+exports.updateTeam = async (req, res, next) => {
+    try {
+        const team = await Team.findByIdAndUpdate(req.params.id, req.body, { // We are find the entry by id and the pass what we want to inser with req.body
+            new: true,
+            runValidators: true 
+        });
+        if(!team) {
+            return res.status(400).json({success:false});
+        }
+
+        res.status(200).json({success: true, data: team})
+    } catch (error) {
+        res.status(400).json({success: false}); 
+    }
 }
 
 
 // @desc        delete a team
 // @route       DELETE /api/v1/teams/:id
 // @access      Public
-exports.deleteTeam = (req, res, next) => {
-    res
-        .status(200)
-        .json({success: true, msg: `Delete team ${req.params.id}`})
-    
+exports.deleteTeam = async (req, res, next) => {
+    try {
+        const team = await Team.findByIdAndDelete(req.params.id)
+
+        if (!team) {
+            return res.status(400).json({success:false});
+        }
+
+        res.status(200).json({success: true, data: {}});
+    } catch (error) {
+        res.status(400).json({success: false});
+    }
 }
