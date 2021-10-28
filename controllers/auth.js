a0f56d13a9fa24d6dc1000a93e028ad472b14575
salramm <1salmanov.ramil@gmail.com>
2021-10-27 21:22:33 -0400

    Adding forgot password and reset password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
+const sendEmail = require('../utils/sendEmail');
+const crypto = require('crypto');
 
 // @desc      Register User
 // @route     POST /api/v1/auth/register
@@ -75,16 +77,66 @@ exports.forgotPassword = asyncHandler(async (req, res, next ) => {
     //Get the reset token
     const resetToken = user.getResetPasswordToken();
 
-    await user.save({ validateBeforeSave: false});
+    await user.save({ validateBeforeSave: false });
 
-    res.status(200).json({
-        success: true,
-        data: user
-    })
+    // Create reset URL
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/auth/resetpassword/${resetToken}`;
+
+    const message = `You are recieving this email bc a password reset was requested. Please make a 
+    PUT request to: \n\n ${resetUrl}`;
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: 'Password reset token',
+            message
+        });
+
+        res.status(200).json({ success: true, data: 'Email sent'});
+
+    } catch (err) {
+        console.log(err);
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save({ validateBeforeSave: false});
+
+        return next(new ErrorResponse('Email could not be sent', 500));
+    }
+
 });
 
+// @desc      Reset Password
+// @route     PUT /api/v1/auth/resetpassword/:resettoken
+// @access    Public
+
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+
+    //Get hashed token
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.resettoken).digest('hex');
 
 
+    // Get the user by the token he sent
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now()}
+    });
+
+    if (!user) {
+        return next(new ErrorResponse('Invalid Token', 400));
+    }
+
+    // Set the new passowrd
+    user.password = req.body.password;
+
+    //Clear the fields used to reset
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+    
+    sendTokenResponse(user, 200, res);
+});
 
 //Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const { register, login, getMe, forgotPassword } = require('../controllers/auth');
+const { register, login, getMe, forgotPassword, resetPassword } = require('../controllers/auth');
 const User = require('../models/User');
 
 const router = express.Router();
 
-const {protect } = require('../middleware/auth')
+const { protect } = require('../middleware/auth')
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
 router.post('/forgotpassword', forgotPassword);
+router.put('/resetpassword/:resettoken', resetPassword);
 
 module.exports = router;
 
