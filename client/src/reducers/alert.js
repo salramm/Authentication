deb5732c3690ca74967a58c2063ba2957b620815
salramm <1salmanov.ramil@gmail.com>
2021-10-31 15:25:57 -0400

    adding redux, lerning reducers, adding alert reducer

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,3 +1,9 @@
+import { SET_ALRT, REMOVE_ALERT } from '../actions/types'
+
 const initialState = [];
 
-export default function(state = initialState, action)
+export default function(state = initialState, action) {
+    switch(action.type) {
+        case 
+    }
+}
