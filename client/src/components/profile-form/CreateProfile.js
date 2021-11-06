731cd8f529c8785230e35f8c32ab8a74644c35f4
salramm <1salmanov.ramil@gmail.com>
2021-11-06 15:01:24 -0400

    created profile create/edit action, will change to league

--- client/src/components/profile-form/CreateProfile.js
+++ client/src/components/profile-form/CreateProfile.js
@@ -1,8 +1,11 @@
 import React, {Fragment, useState} from 'react'
+import {Link, withRouter} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { createProfile } from '../../actions/profile'
 
-const CreateProfile = props => {
+
+const CreateProfile = ({createProfile, history}) => {
 
     const [formData, setFormData] = useState({   //Paste all the fields in here
         company: '',
@@ -18,6 +21,11 @@ const CreateProfile = props => {
 
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
 
+    const onSubmit = e => {
+        e.preventDefault();
+        createProfile(formData, history)
+    }
+
     return (
         <Fragment>
                     <h1 className="large text-primary">
@@ -28,7 +36,7 @@ const CreateProfile = props => {
                 profile stand out
             </p>
             <small>* = required field</small>
-            <form className="form">
+            <form className="form" onSubmit={e => onSubmit(e)}>
                 <div className="form-group">
                 <select name="status" value={status} onChange={e => onChange(e)}>
                     <option value="0">* Select Professional Status</option>
@@ -129,8 +137,8 @@ const CreateProfile = props => {
 
 
 CreateProfile.propTypes = {
-
+    createProfile: PropTypes.func.isRequired,
 }
 
 
-export default CreateProfile
+export default connect(null, {createProfile}) (withRouter (CreateProfile));
